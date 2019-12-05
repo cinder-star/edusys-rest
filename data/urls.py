25fa8675048ede8ed858371bcc8edd11ff479f5b
SedativeHypnotics <sihantawsik@gmail.com>
2019-12-05 18:51:03 +0600

    changed response type and added find student

--- data/urls.py
+++ data/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("all_students/", views.all, name="all_students"),
+    path("find_student/",views.find,name="find_student"),
 ]
--- data/views.py
+++ data/views.py
@@ -1,17 +1,25 @@
 from django.shortcuts import render
-from django.http import Http404, JsonResponse
+from django.http import HttpResponseNotFound, JsonResponse, HttpResponse
 from .serializers import JSONSerializer
 from .models import StudentsData
 import json
 
 # Create your views here.
 
+def find(request):
+    uid = request.GET['uid']
+    try:
+        student = StudentsData.objects.get(uid=uid)
+    except:
+        return HttpResponseNotFound()
+
+    return HttpResponse()
 
 def all(request):
     try:
         studentsdata = StudentsData.objects.all()
     except:
-        raise Http404("Not found")
+        return HttpResponseNotFound()
     serializers = JSONSerializer()
     students = serializers.serialize(studentsdata)
     return JsonResponse(students, safe=False)
